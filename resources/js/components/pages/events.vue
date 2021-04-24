d68dda6430c4db1b777bcddc25d1b7bd570ad411
unknown <khechinibakr20@gmail.com>
2021-04-24 04:51:14 +0200

    event and forum page

--- resources/js/components/pages/events.vue
+++ resources/js/components/pages/events.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        <h1>EVENEMENTS</h1>
+    <div class="events-page">
+        <div class="container">
+            <div class="calendrier">
+                <h2>Calendrier des évènements</h2>
+            </div>
+            <div class="last-events">
+                <h2>Nos Derniers évènements</h2>
+                <div class="card" style="width: 18rem;">
+                <img src="#" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
--- resources/js/components/pages/forum.vue
+++ resources/js/components/pages/forum.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        <h1>Forum</h1>
+    <div class="forum">
+        <div class="container">
+            <div class="login">
+                
+            </div>
+            <div class="forum-section">
+                <h2>Forum</h2>
+                <div class="card" style="width: 18rem;">
+                <img src="#" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
