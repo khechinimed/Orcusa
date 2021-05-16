8852fcbff7d55d133bc4b6459e5e5c24f94a9b03
unknown <khechinibakr20@gmail.com>
2021-05-16 03:42:59 +0200

    fichier de migration du calendrier

---
+++ database/migrations/2021_05_16_010920_create_calendars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalendarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendars', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_name');
+            $table->string('event_image')->nullable();
+            $table->text('event_description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendars');
+    }
+}
