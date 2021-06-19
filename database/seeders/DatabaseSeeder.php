fb67db85e3412ccbab79a1d2c83ce9619b44b369
unknown <khechinibakr20@gmail.com>
2021-06-19 15:47:38 +0200

    Users and FAQ Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'FullName' => 'Mathias Pora',
+            'email' => 'orcusa@gmail.com',
+            'password' => bcrypt('password'),
+            'userType' => 'admin'
+        ]);
+
+        DB::table('f_a_q_s')->insert([
+            'title' => '1',
+            'text' => '2'
+        ]);
+        DB::table('f_a_q_s')->insert([
+            'title' => '1',
+            'text' => '2'
+        ]);
+        DB::table('f_a_q_s')->insert([
+            'title' => '1',
+            'text' => '2'
+        ]);
+        DB::table('f_a_q_s')->insert([
+            'title' => '1',
+            'text' => '2'
+        ]);
+        DB::table('f_a_q_s')->insert([
+            'title' => '1',
+            'text' => '2'
+        ]);
     }
 }
